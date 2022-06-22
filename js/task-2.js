4fa6b406fc3f926e340a37f32a8eea5655d6b776
Mykola Kovalchuk <kolyankov@gmail.com>
2022-06-22 21:38:18 +0300

    add task-2

---
+++ js/task-2.js
@@ -0,0 +1,19 @@
+const calculateEngravingPrice = function (message, pricePerWord) {
+  const messageArray = message.split(" ");
+  const priceAll = messageArray.length * pricePerWord;
+
+  return priceAll;
+};
+
+
+console.log(
+  calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
+
+console.log(
+  calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
